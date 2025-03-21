2a15e399e369b4fe53be7adedf8dd631efcc880a
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2025-03-21 12:20:26 -0300

    Remove SharedModule

--- src/shared/shared.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
-
-@Module({
-  imports: [],
-  providers: [PrismaService],
-})
-export class SharedModule {}
